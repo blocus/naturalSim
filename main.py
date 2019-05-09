60f2c3888537ee7fa87003f2e037cab25bef348f
Ahmed Meftah <ahmed.meftah1993@gmail.com>
2019-05-09 03:17:28 +0100

    use food instead of world concept

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
+import random
+import time
 from tkinter import *
-from world import World
+from pixie import Pixie
+from food import Food
+from utils import *
+
+seed = int(time.time())
+random.seed(seed)
+print("Seed for world =", seed)
 
 ## global vars
-canvasWidth = 530
-canvasHeight = 530
+canvasWidth = 1270
+canvasHeight = 790
 
 
 ## Tkinter Stuff
 root = Tk()
 c = Canvas(root, width=canvasWidth, height=canvasHeight)
-
-
+pixies = []
+foods = []
 ## call class
-worldMap = World(c, canvasWidth, canvasHeight)
-
-
+c.create_rectangle(0, 0, canvasWidth, canvasHeight, fill="#19722f")
+for i in range(10):
+    x = random.random() * canvasWidth
+    y = random.random() * canvasHeight
+    pixies.append(Pixie(c, x, y))
 c.pack()
 
 # the simulation
 while True:
-    worldMap.show(c)
+    if len(foods) < 1000:
+        foods.append(Food(c))
+
+    for pixie in pixies:
+        pixie.goTo(c, foods)
     c.update()
 
 root.mainloop()
